639eaa1ff9ecbd9700e8bc623deb973f8fb92d4f
Drew Tolliver <dtolliver@Drews-MacBook-Pro.local>
2017-04-18 16:30:09 -0400

    added replay capability to number guessing game

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,22 +1,37 @@
 import random
 
 def play():
-  target_number = random.randint(1, 10)
-  player_guess = 0
-  guess_count = 5
+  
   print "I am thinking of a number between 1 and 10."
-  while player_guess != target_number and guess_count > 0:
-      print "You have {} guesses left.".format(guess_count)
-      player_guess = input("What's the number? ")
-      if int(player_guess) < target_number:
-          print "{} is too low.".format(player_guess)
-      elif int(player_guess) > target_number:
-          print "{} is too high.".format(player_guess)
-      else:
-          print "Yes! You Win!"
-      guess_count -= 1
-      if guess_count == 0:
-        print "Sorry, you did not successfully guess the number :-("
+  while True:
+    target_number = random.randint(1, 10)
+    player_guess = 0
+    guess_count = 5
+    replay = ""
+    while player_guess != target_number and guess_count > 0:
+        print "You have {} guesses left.".format(guess_count)
+        player_guess = input("What's the number? ")
+        if int(player_guess) < target_number:
+            print "{} is too low.".format(player_guess)
+        elif int(player_guess) > target_number:
+            print "{} is too high.".format(player_guess)
+        else:
+            print "Yes! You Win!"
+        guess_count -= 1
+        if guess_count == 0:
+            print "Sorry, you did not successfully guess the number :-("
+
+    replay = raw_input("Would you like to play again (y or n )? ")
+    if replay == "n":
+        print "Bye!"
+        return
+    elif replay == "y":
+        print "I am thinking of a number between 1 and 10."
+    else:
+        print "Please enter y or n."
+        
+        
+
 
 # def play_again():
 #     replay = raw_input("Would you like to play again (Y or N)? ")
