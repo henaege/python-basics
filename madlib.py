a3d3bf51a3f97aeea86926ddb7a30b6f382e09c4
Drew Tolliver <dtolliver@Drews-MacBook-Pro.local>
2017-04-18 16:59:43 -0400

    adding madlib.py

---
+++ madlib.py
@@ -0,0 +1,5 @@
+print """Please fill in the blanks below: 
+____(name)____'s favorite subject in school is ____(subject)____."""
+name = raw_input("What is name? ")
+subject = raw_input("What is subject? ")
+print "{}'s favorite subject in school is {}.".format(name, subject)
